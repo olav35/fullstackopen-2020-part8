4535a7340e68be3044d5585c978939bcd59ee169
Olav Fosse <fosseolav@gmail.com>
2020-09-13 13:55:42 +0200

    Implement books view

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,32 +1,46 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+    }
+  }
+`
 
 const Books = () => {
-  const books = []
+  const result = useQuery(ALL_BOOKS)
 
   return (
     <div>
       <h2>books</h2>
-
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
-          </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      {
+        result.loading ? <div>Loading...</div> : (
+          <table>
+            <tbody>
+              <tr>
+                <th></th>
+                <th>
+                  author
+                </th>
+                <th>
+                  published
+                </th>
+              </tr>
+              {result.data.allBooks.map(a =>
+                <tr key={a.title}>
+                  <td>{a.title}</td>
+                  <td>{a.author}</td>
+                  <td>{a.published}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        )
+      }
     </div>
   )
 }
