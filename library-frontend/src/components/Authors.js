0eb366d97ede1d6af97dcacce544bdb0e127690e
Olav Fosse <fosseolav@gmail.com>
2020-09-13 13:52:40 +0200

    Implement authors view

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,33 +1,47 @@
   
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
 
 const Authors = () => {
-  const authors = []
+  const result = useQuery(ALL_AUTHORS)
 
   return (
     <div>
       <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
-          </tr>
-          {authors.map(a =>
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-
+      {
+        result.loading ? <div>Loading...</div> : (
+          <table>
+            <tbody>
+              <tr>
+                <th></th>
+                <th>
+                  born
+                </th>
+                <th>
+                  books
+                </th>
+              </tr>
+              {result.data.allAuthors.map(a =>
+                <tr key={a.name}>
+                  <td>{a.name}</td>
+                  <td>{a.born}</td>
+                  <td>{a.bookCount}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        )
+      }
     </div>
   )
 }
